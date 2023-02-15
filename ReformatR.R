f460264f53f62f621690b849a8b8ee97f3b59be0
Andrew Perl <119334267+Perl-AT@users.noreply.github.com>
2023-02-15 15:24:04 -0500

    Update ReformatR.R
    Added functionality for both "Pct Component Freezing" and "Avg Motion Index" data for all experiment types.

--- ReformatR.R
+++ ReformatR.R
@@ -1,9 +1,9 @@
-# Updated 01.31.2023 by Andrew Perl
+# Updated 02.15.2023 by Andrew Perl
 
 library(shiny)
 
 # Layout of user interface
-ui <- fluidPage(navbarPage(title="ReformatR 1.1", 
+ui <- fluidPage(navbarPage(title="ReformatR 1.2", 
   
   # GUI layout
   tabPanel("GUI", 
@@ -47,7 +47,7 @@ ui <- fluidPage(navbarPage(title="ReformatR 1.1",
   
   # Usage tab layout
   tabPanel("Usage", 
-           h1(a(href="https://github.com/Perl-AT/ReformatR", "ReformatR 1.1")), 
+           h1(a(href="https://github.com/Perl-AT/ReformatR", "ReformatR 1.2")), 
            p("This ", a(href="https://www.rdocumentation.org/packages/shiny", "shiny"), "app takes, as input, .csv reports from Med Associates ", a(href="https://med-associates.com/product/videofreeze-video-fear-conditioning-software/", "Video Freeze"), a(href="https://med-associates.com/wp-content/uploads/2022/09/DOC-321-R1.1-SOF-843-USB-Video-Freeze.pdf", "(manual)"), "and makes, as output, new .csv files containing only key data from the input file, reformatted so as to be readily handleable in R and SPSS."), 
            hr(), 
            h3("Usage"), 
@@ -97,7 +97,7 @@ ui <- fluidPage(navbarPage(title="ReformatR 1.1",
            h3("Getting Help"), 
            p("Reach out to the maintainer for help or to make suggestions, or submit issues or pull requests on ", a(href= "https://github.com/Perl-AT/ReformatR", "GitHub", .noWS="after"), ". Accommodation for new experiment types can be added into the existing framework ", tags$i("ad libitum", .noWS="after"), "."), 
            hr(), 
-           h5("Author/Maintainer: Andrew Perl (perlat@nih.gov; atperl123@gmail.com; ", a(href="https://github.com/Perl-AT", "Perl-AT", .noWS="after"), ")")
+           h5("Author/Maintainer: Andrew Perl (perlat@nih.gov | atperl123@gmail.com | ", a(href="https://github.com/Perl-AT", "Perl-AT", .noWS="after"), ")")
            )
   
   )
@@ -111,16 +111,16 @@ server <- function(input, output) {
     file1 <- input$file
     FirDF <- read.csv(file1$datapath, header=FALSE, col.names=c(1:30))
     SecDF <- data.frame()
-    SecDF[1,1:4] <- c("test number", "animal", "group", "box")
-    OP <- 0
+    SecDF[1,1:6] <- c("test number", "animal", "group", "box")
       
     # Reformatting instructions for Acquisition/Tone Test
     if (input$radio == 1) {
-      SecDF[1,5:17] <- FirDF[21:33,6]
-      SecDF[1,18:20] <- c("Tone_AVG", "Shock_AVG", "Trace_AVG")
+      SecDF[1,5:17] <- paste0("Freezing_", FirDF[21:33,6])
+      SecDF[1,18:20] <- paste0("Freezing_", c("Tone_AVG", "Shock_AVG", "Trace_AVG"))
+      SecDF[1,21:33] <- paste0("Motion_", FirDF[21:33,6])
+      SecDF[1,34:36] <- paste0("Motion_", c("Tone_AVG", "Shock_AVG", "Trace_AVG"))
       n <- input$NoM
-      width <- 20
-      OP <- 1
+      width <- 36
       type <- "Acquisition/Tone Test"
       pro <- "Protocol file: '3 Tone Acquisition 75 Sheryl protocol.pro'"
       cmp <- "Component file: '3 TS DelayToneAquandTest Sheryl.cmp'"
@@ -129,22 +129,27 @@ server <- function(input, output) {
         SecDF[(i+1),2] <- FirDF[(21+(13*(i-1))),4]
         SecDF[(i+1),3] <- FirDF[(21+(13*(i-1))),5]
         SecDF[(i+1),4] <- FirDF[(21+(13*(i-1))),3]
+        # freezing data
         SecDF[(i+1),5:17] <- FirDF[(21:33+(13*(i-1))),10]
-        SecDF[(i+1),7] <- FirDF[(23+(13*(i-1))),11]
-        SecDF[(i+1),10] <- FirDF[(26+(13*(i-1))),11]
-        SecDF[(i+1),13] <- FirDF[(29+(13*(i-1))),11]
         SecDF[(i+1),18] <- mean(as.numeric(SecDF[(i+1),c(6,9,12)]))
         SecDF[(i+1),19] <- mean(as.numeric(SecDF[(i+1),c(7,10,13)]))
         SecDF[(i+1),20] <- mean(as.numeric(SecDF[(i+1),c(15,16,17)]))
+        # motion data
+        SecDF[(i+1),21:33] <- FirDF[(21:33+(13*(i-1))),11]
+        SecDF[(i+1),34] <- mean(as.numeric(SecDF[(i+1),c(22,25,28)]))
+        SecDF[(i+1),35] <- mean(as.numeric(SecDF[(i+1),c(23,26,29)]))
+        SecDF[(i+1),36] <- mean(as.numeric(SecDF[(i+1),c(31,32,33)]))
       }
     }
       
     # Reformatting instructions for 5 min/30 sec Context Test
     if (input$radio == 2) {
-      SecDF[1,5:14] <- FirDF[18:27,6]
-      SecDF[1,15] <- "AVG"
+      SecDF[1,5:14] <- paste0("Freezing_", FirDF[18:27,6])
+      SecDF[1,15] <- "Freezing_AVG"
+      SecDF[1,16:25] <- paste0("Motion_", FirDF[18:27,6])
+      SecDF[1,26] <- "Motion_AVG"
       n <- input$NoM
-      width <- 15
+      width <- 26
       type <- "5 min/30 sec Context Test"
       pro <- "Protocol file: 'Context Test 5 min.pro'"
       cmp <- "Component file: '5minContext30secbinsFIXED.cmp'"
@@ -153,17 +158,22 @@ server <- function(input, output) {
         SecDF[(i+1),2] <- FirDF[(18+(10*(i-1))),4]
         SecDF[(i+1),3] <- FirDF[(18+(10*(i-1))),5]
         SecDF[(i+1),4] <- FirDF[(18+(10*(i-1))),3]
+        # freezing data
         SecDF[(i+1),5:14] <- FirDF[(18:27+(10*(i-1))),10]
         SecDF[(i+1),15] <- mean(as.numeric(FirDF[(18:27+(10*(i-1))),10]))
+        # motion data
+        SecDF[(i+1),16:25] <- FirDF[(18:27+(10*(i-1))),11]
+        SecDF[(i+1),26] <- mean(as.numeric(FirDF[(18:27+(10*(i-1))),11]))
         }
       }
     
     # Reformatting instructions for 5 min/1 min Context Test
     # Note: during tests ReformatR did not handle empty rows in the input .csv the way it has for other inputs (i.e. it did not ignore empty rows) -- this was for both tested input files. Keep an eye on this test... 
     if (input$radio == 3) {
-      SecDF[1,5:10] <- FirDF[18:23,6]
+      SecDF[1,5:10] <- paste0("Freezing_", FirDF[18:23,6])
+      SecDF[1,11:16] <- paste0("Motion_", FirDF[18:23,6])
       n <- input$NoM
-      width <- 10
+      width <- 16
       type <- "5 min/1 min Context Test"
       pro <- "Protocol file: '5 min context test Days 4 and 5.pro'"
       cmp <- "Component file: '5Minute context test.cmp'"
@@ -172,7 +182,10 @@ server <- function(input, output) {
         SecDF[(i+1),2] <- FirDF[(18+(7*(i-1))),4]
         SecDF[(i+1),3] <- FirDF[(18+(7*(i-1))),5]
         SecDF[(i+1),4] <- FirDF[(18+(7*(i-1))),3]
+        # freezing data
         SecDF[(i+1),5:10] <- FirDF[(18:23+(7*(i-1))),10]
+        # motion data
+        SecDF[(i+1),11:16] <- FirDF[(18:23+(7*(i-1))),11]
       }
     }
     
@@ -182,7 +195,6 @@ server <- function(input, output) {
       SecDF[1,16:26] <- paste0("Motion_", FirDF[19:29,6])
       n <- input$NoM
       width <- 26
-      OP <- 2
       type <- "10 min Pre-Exposure"
       pro <- "Protocol file: '10 min pre-exposure Days 1 and 2.pro'"
       cmp <- "Component file: '10 min preexposure.cmp'"
@@ -204,7 +216,6 @@ server <- function(input, output) {
       SecDF[1,8:10] <- paste0("Motion_", FirDF[11:13,6])
       n <- input$NoM
       width <- 10
-      OP <- 2
       type <- "Immediate Shock"
       pro <- "Protocol file: 'Immediate Shock 10sec PSI Day 3.pro'"
       cmp <- "Component file: 'immediate shock 10s PSI.cmp'"
@@ -223,18 +234,10 @@ server <- function(input, output) {
     # Footer information
     SecDF2 <- SecDF
     SecDF2[(n+2:9),1:width] <- ""
-    SecDF2[(n+3),1] <- "ReformatR version: 1.1"
-    SecDF2[(n+4),1] <- paste0("Date/time: ", as.character(Sys.time()))
+    SecDF2[(n+3),1] <- "ReformatR version: 1.2"
+    SecDF2[(n+4),1] <- paste("Reformatting date/time:", as.character(Sys.time()))
     SecDF2[(n+6:8),1] <- c(type, pro, cmp)
-    if (OP == 0) {
-      SecDF2[(n+9),1] <- "Output values for all components are 'Pct Component Time Freezing.'"
-    }
-    if (OP == 1) {
-      SecDF2[(n+9),1] <- "Output values for all three shocks are 'Avg Motion Index,' and output values for all other components are 'Pct Component Time Freezing.'"
-    }
-    if (OP == 2) {
-      SecDF2[(n+9),1] <- "Output values in 'Freezing_' columns are 'Pct Component Time Freezing,' while values in 'Motion_' columns are 'Avg Motion Index.'"
-    }
+    SecDF2[(n+9),1] <- "Output values in 'Freezing_' columns are 'Pct Component Time Freezing,' while values in 'Motion_' columns are 'Avg Motion Index.'"
     
     SecDF2 <<- SecDF2
     SecDF <<- SecDF
